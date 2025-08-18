d6da45aadec9a4c62cb8c223e9c4f7afccfca033
luiscarlosjp <jardimpereiralc@gmail.com>
2025-08-17 23:50:00 -0300

    test: add CPU stress test to evauate autoscaling behavior

--- app-ts/src/app.service.ts
+++ app-ts/src/app.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { createWriteStream } from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(process.env.APP);
-    console.log(process.env.API_KEY);
+    console.log(`ConfigMap: ${process.env.APP}`);
+    console.log(`Secret: ${process.env.API_KEY}`);
     return 'Hello World!';
   }
-  
+
   getExample(): string {
-    return 'Estou rodando no K8s';
+    const file = createWriteStream('rocketset.txt');
+    for (let x = 0; x < 10000; x++) {
+      file.write('Estou escrevendo em um arquivo\n');
+    }
+    return 'Estou rodando no K8S';
   }
 }
